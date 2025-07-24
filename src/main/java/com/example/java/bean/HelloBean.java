8ed8911a190ed56323d2e97f76f8dae8595cbaa4
dela-hashem <dela.hashem89@gmail.com>
2025-07-24 21:40:19 +0800

    Initial commit: working JSF + PrimeFaces setup

---
+++ src/main/java/com/example/java/bean/HelloBean.java
@@ -0,0 +1,26 @@
+package com.example.java.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@RequestScoped
+public class HelloBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // متدی که با دکمه فراخوانی میشه
+    public void sayHello() {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", "Hello " + name + "!"));
+    }
+}
